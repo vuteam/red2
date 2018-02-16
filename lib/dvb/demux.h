acab8e1bc213de175857a8c4f6cc977882eab234
vuteam <rakzami39@gmail.com>
2018-02-16 12:30:06 +0100

    Update demux.h

--- lib/dvb/demux.h
+++ lib/dvb/demux.h
@@ -43,16 +43,16 @@ private:
 	friend class eDVBTText;
 	friend class eDVBTSRecorder;
 	friend class eDVBCAService;
-	Signal1<void, int> m_event;
+	sigc::signal1<void, int> m_event;
 	
 	int openDemux(void);
 };
 
-class eDVBSectionReader: public iDVBSectionReader, public Object
+class eDVBSectionReader: public iDVBSectionReader, public sigc::trackable
 {
 	DECLARE_REF(eDVBSectionReader);
 	int fd;
-	Signal1<void, const __u8*> read;
+	sigc::signal1<void, const __u8*> read;
 	ePtr<eDVBDemux> demux;
 	int active;
 	int checkcrc;
@@ -64,14 +64,14 @@ public:
 	RESULT setBufferSize(int size);
 	RESULT start(const eDVBSectionFilterMask &mask);
 	RESULT stop();
-	RESULT connectRead(const Slot1<void,const __u8*> &read, ePtr<eConnection> &conn);
+	RESULT connectRead(const sigc::slot1<void,const __u8*> &read, ePtr<eConnection> &conn);
 };
 
-class eDVBPESReader: public iDVBPESReader, public Object
+class eDVBPESReader: public iDVBPESReader, public sigc::trackable
 {
 	DECLARE_REF(eDVBPESReader);
 	int m_fd;
-	Signal2<void, const __u8*, int> m_read;
+	sigc::signal2<void, const __u8*, int> m_read;
 	ePtr<eDVBDemux> m_demux;
 	int m_active;
 	void data(int);
@@ -82,7 +82,7 @@ public:
 	RESULT setBufferSize(int size);
 	RESULT start(int pid);
 	RESULT stop();
-	RESULT connectRead(const Slot2<void,const __u8*, int> &read, ePtr<eConnection> &conn);
+	RESULT connectRead(const sigc::slot2<void,const __u8*, int> &read, ePtr<eConnection> &conn);
 };
 
 class eDVBRecordFileThread: public eFilePushThread
@@ -105,7 +105,7 @@ private:
 	int m_pid;
 };
 
-class eDVBTSRecorder: public iDVBTSRecorder, public Object
+class eDVBTSRecorder: public iDVBTSRecorder, public sigc::trackable
 {
 	DECLARE_REF(eDVBTSRecorder);
 public:
@@ -129,7 +129,7 @@ public:
 
 	RESULT getCurrentPCR(pts_t &pcr);
 
-	RESULT connectEvent(const Slot1<void,int> &event, ePtr<eConnection> &conn);
+	RESULT connectEvent(const sigc::slot1<void,int> &event, ePtr<eConnection> &conn);
 private:
 	RESULT startPID(int pid);
 	void stopPID(int pid);
@@ -138,7 +138,7 @@ private:
 	void filepushEvent(int event);
 	
 	std::map<int,int> m_pids;
-	Signal1<void,int> m_event;
+	sigc::signal1<void,int> m_event;
 	
 	ePtr<eDVBDemux> m_demux;
 	
